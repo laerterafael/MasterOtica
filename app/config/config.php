be3f4fdf396673b887bfa4f7cefd2477417b5426
Laerte Rafael <laerterafael.ads@gmail.com>
2021-03-01 15:21:50 -0300

    Iniciando a estrutura do projeto

---
+++ app/config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+define('BASE', '/MasterOtica/');
+define('UNSET_URI_COUNT', 1);
+define('DEBUG_URI', TRUE);
---
+++ app/controller/TesteController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\controller;
+
+class TesteController{
+    public  function  seta(){
+        return ['nome' => 'gunnar'];
+    }
+}
